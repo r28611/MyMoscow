a7b3bd11d5d3a3bfb3aade05284e0661a0810915
Margarita Novokh <r28611@gmail.com>
2021-06-14 08:13:41 +0300

    add TabBarController

---
+++ MyMoscow/Flow/Main/AllEvents.swift
@@ -0,0 +1,8 @@
+//
+//  AllEvents.swift
+//  MyMoscow
+//
+//  Created by Margarita Novokhatskaia on 13.06.2021.
+//
+
+import Foundation
---
+++ MyMoscow/Flow/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  MyMoscow
+//
+//  Created by Margarita Novokhatskaia on 13.06.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
